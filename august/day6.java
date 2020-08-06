628248c13169a078ab566c311f74f675b86b07b2
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-08-06 08:16:06 -0400

    day6 done in java

---
+++ august/day6.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        
+        List<Integer> listOfNumbers = new ArrayList<>();
+        int[] duplicates = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (duplicates[nums[i] - 1] == 0) {
+                System.out.println(duplicates[nums[i] - 1]);
+                duplicates[nums[i] - 1] = 1;
+            } else {
+                listOfNumbers.add(nums[i]);
+            }
+        }
+        return listOfNumbers;
+    }
+}
