7e080edade24848257bac6b122d9c42c6b75acaa
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2020-11-13 22:11:34 -0500

    permutation exercise

--- november/noboring.sql
+++ november/noboring.sql
@@ -1,3 +1,5 @@
+/* https://leetcode.com/problems/not-boring-movies/submissions/ */
+
 SELECT * FROM cinema
 WHERE (id % 2 != 0) AND description != "boring";
 ORDER BY rating DESC;
---
+++ november/validSquare.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/explore/challenge/card/november-leetcoding-challenge/565/week-2-november-8th-november-14th/3527/
+import math
+
+class Solution:
+    def validSquare(self, p1: list, p2: list, p3: list, p4: list) -> bool:
+        pass
+
+    def distance(self, p1: list, p2: list) -> boolean:
+        return [p2[0] - p1[0], p2[1] - p1[1]]
