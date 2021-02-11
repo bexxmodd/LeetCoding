06bd674e522842f004ef96d7aa7d439751b77851
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-02-10 21:32:27 -0500

    flattening doubly linked list with child nodes

--- LinkedList/FlattenDoublyLinkedList.py
+++ LinkedList/FlattenDoublyLinkedList.py
@@ -1,20 +1,26 @@
 # https://leetcode.com/explore/learn/card/linked-list/213/conclusion/1225/
 
 # Definition for a Node.
-class Node:
-    def __init__(self, val, prev, next, child):
-        self.val = val
-        self.prev = prev
-        self.next = next
-        self.child = child
+# class Node:
+#     def __init__(self, val, prev, next, child):
+#         self.val = val
+#         self.prev = prev
+#         self.next = next
+#         self.child = child
+from collections import deque
 
 class Solution:
-    def flatten(self, head: Node) -> 'Node':
-        cur = head
-        tmp = None
-        while cur:
-            if cur.child:
-                cur.next = cur.child
-                cur.prev = cur
-            cur = cur.next
-        return head
+    def flatten(self, head: 'Node') -> 'Node':
+        tmp = head
+        stack = deque()
+        while head:
+            if head.child:
+                if head.next: stack.appendleft(head.next)
+                head.next = head.child
+                head.next.prev = head
+                head.child = None
+            elif head.next is None and len(stack) > 0:
+                head.next = stack.popleft()
+                head.next.prev = head
+            head = head.next
+        return tmp
